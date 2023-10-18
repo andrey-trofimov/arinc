d9766382eda81efb6610c231bf31f21857afa97b
Трофимов Андрей Геннадьевич <tag@szrcai.ru>
2023-10-18 10:34:12 +0300

    add init inputs code

--- src/components/Arinc/Inputs/index.js
+++ src/components/Arinc/Inputs/index.js
@@ -1,25 +1,35 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setRegExp } from "../../../redux/slice/dbSlice";
 import { setStartStr, setEndStr, } from "../../../redux/slice/paginationSlice";
 import "./style.scss";
 
 function Inputs() {
-    let { regExp } = useSelector(state => state.db);
+    let { regExp, partDbMask } = useSelector(state => state.db);
     let { strPerPage } = useSelector(state => state.pagination);
     let dispatch = useDispatch();
 
+    // Заполнение инпутов при смене шаблона
+    useEffect(() => {
+        let testRe = RegExp(".{132}", "gi");
+        let initArr = testRe.test(partDbMask) ? partDbMask : regExp;
+        dispatch(setRegExp(initArr));
+    }, [partDbMask]);
+
+    // Массивы значений для инпутов и стилей инпутов
     let arr = regExp.split("").map(el => el === "." ? "" : el);
     let style = arr.map(el => el !== "" ? "input bg" : "input")
 
     function handlerChange(e, i) {
         let newArr = [...arr];
         newArr.splice(i, 1, String(e.target.value));
-
         newArr = newArr.map(el => el === "" ? "." : el).join("");
-        dispatch(setRegExp(newArr));
 
+        dispatch(setRegExp(newArr));
         dispatch(setStartStr(0));
         dispatch(setEndStr(strPerPage));
+
+        // console.log(newArr)
     }
 
     return (
